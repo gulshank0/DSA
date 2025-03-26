7d5ba3f80ffd3ff38acd001cad902f094e858900
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-26 21:38:22 +0530

    addition quetion done in video-12

---
+++ video-12/MaxNum.java
@@ -0,0 +1,19 @@
+
+public class MaxNum {
+  public static void main(String[] args) {
+    int[] arr = { 12, 98,23, 22, 34, 45 };
+
+    System.out.println(max(arr));
+
+  }
+
+  static int max(int[] arr) {
+    int maxVal = arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > maxVal) {
+        maxVal = arr[i];
+      }
+    }
+    return maxVal;
+  }
+}
