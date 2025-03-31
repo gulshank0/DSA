20ffbe870cbddcedaf3d35f5644311dd23ff30a2
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-31 19:20:58 +0530

    lenear search initialize

--- video-13/linear.java
+++ video-13/linear.java
@@ -1,6 +1,11 @@
+import java.io.Console;
+import java.util.Scanner;
+
 public class linear {
   public static void main(String[] arg) {
-    System.out.println("Hello World");
+    System.out.println("Liner Search will be introduce");
+    Scanner in =new  Scanner(System.in);
+    int n=in.nextInt();
 
   }
 }
---
+++ video-13/linearSearch.java
@@ -0,0 +1,7 @@
+public class linearSearch {
+  public static void main(String[] args) {
+    System.out.println("Here we search across the thing and print out index of that");
+  //started cose from Here
+
+  }
+}
