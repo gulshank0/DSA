042ddb90c6a8569bcb9b8a2d2705f88c9de1e9da
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-31 23:09:33 +0530

    search in ques done

--- video-13/quesOne.java
+++ video-13/quesOne.java
@@ -1,5 +1,21 @@
-public class quesOne{
+public class quesOne {
   public static void main(String[] args) {
-    
+    int[] nums = { 23, 12, 54, 56, 322, 143, 45 };
+    int target = 143;
+    int ans = linearsearches(nums, target, 3, 6);
+    System.out.println(ans);
+  }
+
+  static int linearsearches(int[] arr, int target, int start, int end) {
+    if (arr.length == 0) {
+      return -1;
+    }
+    for (int index = 0; index < end; index++) {
+      int element = arr[index];
+      if (target == element) {
+        return index;
+      }
+    }
+    return -1;
   }
 }
