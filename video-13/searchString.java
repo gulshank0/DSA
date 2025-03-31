78a4f64a71e8d47e8f00c8c16a5cab83ecc39865
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-31 20:40:05 +0530

    search in strings

--- video-13/searchString.java
+++ video-13/searchString.java
@@ -1,5 +1,22 @@
-public class searchString{
+public class searchString {
   public static void main(String[] args) {
-    
+    System.out.println("Searching in the Strings");
+    String name = "Harkirat";
+    char target = 'T';
+    System.out.println(search(name, target));
   }
+
+  static boolean search(String str, char target) {
+    if (str.length() == 0) {
+      return false;
+    }
+    for (int index = 0; index < str.length(); index++) {
+      if (str.charAt(index) == target) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
 }
