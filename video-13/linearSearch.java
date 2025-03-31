009b727b28798994eb918efb65f609087657514f
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-31 19:51:03 +0530

    lenear search ques-1

--- video-13/linearSearch.java
+++ video-13/linearSearch.java
@@ -1,7 +1,25 @@
 public class linearSearch {
   public static void main(String[] args) {
     System.out.println("Here we search across the thing and print out index of that");
-  //started cose from Here
+    // started code from Here
+    {
+      int[] nums= { 23, 445, 6, 33, 124, 56, 322, 56 };
+      int target = 322;
+      int ans = linearsearches(nums, target);
+      System.out.println(ans);
+    }
+  }
 
+  static int linearsearches(int[] arr, int target) {
+    if (arr.length == 0) {
+      return -1;
+    }
+    for (int index = 0; index < arr.length; index++) {
+      int element = arr[index];
+      if (target == element) {
+        return index;
+      }
+    }
+    return -1;
   }
 }
