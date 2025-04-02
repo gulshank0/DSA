00d88c682381b578a00cfb3257de95e1d82b10f5
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-03 00:07:59 +0530

    binarysearch question solved

--- video-14/another.java
+++ video-14/another.java
@@ -1,13 +1,11 @@
-import java.util.Scanner;
-
-public class binarySearch {
+public class another {
   public static void main(String[] args) {
-    System.out.println("Enter the target value to search as binarySearch");
-    Scanner in = new Scanner(System.in);
-    int[] nums = { 23, 44, 56, 76, 88, 90 };
-    int inputarget = in.nextInt();
-    int ans = BinarySearch(nums, inputarget);
-    System.out.println("The Index of input value is :"+" " + ans);
+
+    int[] nums = { 2, 3, 22, 33, 45, 47, 88, 99, 700 };
+    int target = 88;
+    int ans = BinarySearch(nums, target);
+    System.out.println(" The Index of the target value is : "+ans);
+
   }
 
   // return -1 if it does not exist
@@ -20,10 +18,12 @@ public class binarySearch {
         end = mid - 1;
       } else if (target > arr[mid]) {
         start = mid + 1;
+
       } else {
         return mid;
       }
     }
     return -1;
   }
+
 }
