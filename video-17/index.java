d99183fd8c11043c83156cad9316413b4879d213
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-07 22:37:29 +0530

    another question done

--- video-17/index.java
+++ video-17/index.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
+
 public class index {
   public static void main(String[] args) {
     System.out.println("Here we gone perform bubble sort");
+    int[] nums = { 12, 36, 22, 45, 65, 55, 43, 1 };
+    bubble(nums);
+    System.out.println(Arrays.toString(nums));
+  }
 
+  static void bubble(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 1; j < arr.length - i; j++) {
+        // swap the item is smaller than the previous items
+        if (arr[j] < arr[j - 1]) {
+          // swap
+          int temp = arr[j];
+          arr[j] = arr[j - 1];
+          arr[j - 1] = temp;
+        }
+      }
+    }
   }
 }
