96f841feca72a0f42af7b4431a54a22193968ae2
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-07 23:11:37 +0530

    another question done

--- video-17/index.java
+++ video-17/index.java
@@ -9,15 +9,21 @@ public class index {
   }
 
   static void bubble(int[] arr) {
+    boolean swapped;
     for (int i = 0; i < arr.length; i++) {
+      swapped = false;
       for (int j = 1; j < arr.length - i; j++) {
-        // swap the item is smaller than the previous items
         if (arr[j] < arr[j - 1]) {
           // swap
           int temp = arr[j];
           arr[j] = arr[j - 1];
           arr[j - 1] = temp;
+          swapped = true;
         }
+
+      }
+      if (!swapped) {
+        break;
       }
     }
   }
