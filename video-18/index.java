3c155ca3d1dd921e3686a267286434087b0b0a4c
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-08 21:35:35 +0530

    another question done

--- video-18/index.java
+++ video-18/index.java
@@ -1,5 +1,10 @@
 public class index {
   public static void main(String[] args) {
     System.out.println("Here we gone perform Selection sort");
+    int[] nums = { 1, 43, 2, 23, 44, 55, 43, 88 };
+  }
+
+  static int sort(int[] arr){
+    jhjk
   }
 }
