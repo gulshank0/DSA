7a2f442e6935ef9c3a3e9824528f7796cf04fff0
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-10 20:21:19 +0530

    file added

---
+++ video-20/index.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class index {
+  public static void main(String[] args) {
+    System.out.println("CycleSort will be performed Here");
+    int[] nums = { 1, 6, 54, 22, 12, 78, 66, 33, 88, 333, 11, 33 };
+    CycleSort(nums);
+    System.out.println(Arrays.toString(nums));
+  }
+
+  static void CycleSort(int[] arr) {
+
+  }
+}
