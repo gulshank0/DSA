abc3a575edf5f3610bd5c87c651ac7effb809965
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-10 22:56:26 +0530

    question solved

--- video-20/index.java
+++ video-20/index.java
@@ -3,12 +3,26 @@ import java.util.Arrays;
 public class index {
   public static void main(String[] args) {
     System.out.println("CycleSort will be performed Here");
-    int[] nums = { 1, 6, 54, 22, 12, 78, 66, 33, 88, 333, 11, 33 };
+    int[] nums = { 3, 5, 2, 1, 4 };
     CycleSort(nums);
     System.out.println(Arrays.toString(nums));
   }
 
   static void CycleSort(int[] arr) {
+    int i = 0;
+    while (i < arr.length) {
+      int correct = arr[i] - 1;
+      if (arr[i] != arr[correct]) {
+        swap(arr, i, correct);
+      } else {
+        i++;
+      }
+    }
+  }
 
+  static void swap(int[] arr, int first, int second) {
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
   }
 }
