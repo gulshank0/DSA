df7dbf794cdab0ea1d6aee6f6157b9cbc2280a66
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-12 20:06:46 +0530

    file updated

--- video-21/index.java
+++ video-21/index.java
@@ -1,5 +1,10 @@
 public class index {
   public static void main(String[] args) {
     System.out.println("String And String Builders");
+
+    int[] arr = { 2, 3, 4, 5, 6, 322 };
+    int a = 30;
+    String name = "Kunal Kushawaha";
+    System.out.println(name + ": " + a);
   }
 }
