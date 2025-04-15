ca5cb2e5108058b4b859ee9369f02047e77ace12
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-15 22:58:08 +0530

    new qrapper class being revised

--- video-21/PrettyPrinting.java
+++ video-21/PrettyPrinting.java
@@ -11,7 +11,8 @@ public class PrettyPrinting {
     System.out.println('a' + 'b');
     System.out.println("a" + "b");
 
-    System.out.println((char)('a' + 3));
+    System.out.println((char) ('a' + 3));
 
+    System.out.println("a" + 1);
   }
 }
