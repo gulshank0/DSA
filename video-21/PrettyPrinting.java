e08eae47b7daf0aac5fc5b569503b5a84741bd00
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-15 20:40:04 +0530

    new file added

---
+++ video-21/PrettyPrinting.java
@@ -0,0 +1,11 @@
+public class PrettyPrinting {
+  public static void main(String[] args) {
+    System.out.println("Here we gone perform Pretty Printing");
+
+    float a = 23.4567f;
+    // System.out.printf("Formatted number is %.2f", a);
+
+    System.out.println(Math.PI);
+
+  }
+}
