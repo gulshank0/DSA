de229fb0dd235a4d4a7c375c85936f8d29559a84
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-16 23:18:42 +0530

    first pattern

--- video-22/Index.java
+++ video-22/Index.java
@@ -1,5 +1,18 @@
 public class Index {
   public static void main(String[] args) {
     System.out.println("Pattern Printing perform here");
+
+    pattern1(4);
+  }
+
+  static void pattern1(int n) {
+    for (int row = 1; row <= n; row++) {
+      for (int cols = 1; cols <= row; cols++) {
+        System.out.print("* ");
+      }
+      System.out.println(" ");
+
+    }
+
   }
 }
