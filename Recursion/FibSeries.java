918d0302c33261f4f66c490d98d49b2a7d52b19e
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-20 00:38:29 +0530

    another ques done

---
+++ Recursion/FibSeries.java
@@ -0,0 +1,16 @@
+public class FibSeries {
+  public static void main(String[] args) {
+    System.out.println("Printing series of fibonacci");
+  }
+
+  static int[] Fib(int[] n) {
+    if (n < 2) {
+      return n;
+    }
+    for (int i = 0; i < arr.length; i++) {
+      int fibValue = Fib(n - 1) + Fib(n - 2);
+    }
+
+    return fibValue;
+  }
+}
--- Recursion/Fibonnacci.java
+++ Recursion/Fibonnacci.java
@@ -1,8 +1,13 @@
 public class Fibonnacci {
   public static void main(String[] args) {
     System.out.println("Fibonnacci is going to perform");
+    System.out.println(fibb(8));
+  }
 
-  }static void fibb(){
-    
+  static int fibb(int n) {
+    if (n < 2) {
+      return n;
+    }
+    return fibb(n - 1) + fibb(n - 2);
   }
 }
