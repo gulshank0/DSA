199ede0c79770e29715108e276afc24480caf820
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-20 22:48:09 +0530

    ques done

--- Recursion/FibSeries.java
+++ Recursion/FibSeries.java
@@ -1,16 +1,17 @@
 public class FibSeries {
   public static void main(String[] args) {
     System.out.println("Printing series of fibonacci");
+    // int ans = Fib(8);
+    int n = 8;
+    int firstValue = 0;
+    int secondValue = 1;
+
+    for (int i = 0; i < n; i++) {
+      int nextValue = firstValue + secondValue;
+      System.out.println(nextValue);
+      firstValue = secondValue;
+      secondValue = nextValue;
+    }
   }
 
-  static int[] Fib(int[] n) {
-    if (n < 2) {
-      return n;
-    }
-    for (int i = 0; i < arr.length; i++) {
-      int fibValue = Fib(n - 1) + Fib(n - 2);
-    }
-
-    return fibValue;
-  }
 }
---
+++ Recursion/text.java
@@ -0,0 +1,8 @@
+public class text {
+  public static void main(String[] args) {
+    System.out.println("hi there");
+    int[] nums = { 2, 3, 4, 5, 6 };
+    int ans = nums[2];
+    System.out.println(ans);
+  }
+}
