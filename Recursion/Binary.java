3551d98b00e94a442d2160198972b235ce642f20
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-21 01:01:19 +0530

    binary Reccusrion donw

--- Recursion/Binary.java
+++ Recursion/Binary.java
@@ -1,5 +1,22 @@
 public class Binary {
   public static void main(String[] args) {
     System.out.println("Binary Serach using Recurrion");
+    int[] nums = { 2, 3, 5, 6, 7, 8, 9, 44, 45, 66, 77, 88 };
+    int ans = search(nums, 77, 0, nums.length - 1);
+    System.out.println(ans);
+  }
+
+  static int search(int[] arr, int target, int s, int e) {
+    if (s > e) {
+      return -1;
+    }
+    int m = s + (e - s) / 2;
+    if (arr[m] == target) {
+      return m;
+    }
+    if (target < arr[m]) {
+      return search(arr, target, s, m - 1);
+    }
+    return search(arr, target, m + 1, e);
   }
 }
